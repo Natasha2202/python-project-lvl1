d9eea28b9fa4211eaba3e5c5b31c51f1132b729a
Natasha <sterh5@yandex.ru>
2020-08-12 22:58:22 +0300

    Add Makefile

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,12 @@
+import prompt
+
 def main():
     print('Welcome to the Brain Games!')
-
+    welcome_user()
+    
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print ('Hello, {}!'.format(name))
 
 if __name__ == '__main__':
     main()
